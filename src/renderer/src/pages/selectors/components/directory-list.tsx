559021689f618b5e81d4f76da9a0520a31aff199
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-08-11 18:22:06 -0700

    Updated the directories list to utilize the new generic return from the async function hook

--- src/renderer/src/pages/selectors/components/directory-list.tsx
+++ src/renderer/src/pages/selectors/components/directory-list.tsx
@@ -1,28 +1,18 @@
 import { type Dirent } from 'fs';
 
 interface DirectoryListProps {
-  directoriesArray: unknown;
+  directoriesArray: Dirent[];
   asyncFetchError: unknown;
 }
 
 export default function DirectoryList({ directoriesArray, asyncFetchError }: DirectoryListProps): React.JSX.Element {
-  try {
-    if (asyncFetchError) throw new Error('Async Hook Failed');
-    // Confirms that the passed in unknown value is an array containing Dirent elements
-    //? I did this to allow the reuse of the useAsyncFunction hook, this allows for it to return the unknown type but it requires verifying the type before using it.
-    if (directoriesArray instanceof Array) {
-      const values = directoriesArray.map((dir: Dirent) => <div key={dir.name}>{dir.name}</div>);
-
-      return <div>{values}</div>;
-    } else {
-      // Replace with an actual loading icon
-      return <>Loading</>;
-    }
-  } catch (error) {
-    console.error(error);
+  if (asyncFetchError)
     return (
       // Add an actual error page
-      <h1>Error</h1>
+      <h1>{JSON.stringify(asyncFetchError)}</h1>
     );
-  }
+
+  const values = directoriesArray.map((dir: Dirent) => <div key={dir.name}>{dir.name}</div>);
+
+  return <>{values}</>;
 }
