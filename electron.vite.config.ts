6199383f24c9b00990d5a9756111f79c48597c8f
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-08-26 10:52:14 -0700

    Started styling the sorters page

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path';
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import fontawesome from "@fortawesome/react-fontawesome";
 
 export default defineConfig({
   main: {
@@ -19,6 +20,6 @@ export default defineConfig({
         '@': resolve('src/renderer/src')
       }
     },
-    plugins: [react(), tailwindcss()]
+    plugins: [react(), tailwindcss(), ]
   }
 });
--- src/renderer/src/pages/selectors/selectors-page.tsx
+++ src/renderer/src/pages/selectors/selectors-page.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@renderer/components/ui/button';
-import { Card, CardContent, CardFooter } from '@renderer/components/ui/card';
+import { Card, CardContent } from '@renderer/components/ui/card';
 import { Dialog, DialogTrigger } from '@renderer/components/ui/dialog';
 import ButtonLink from '@renderer/components/user/button-link';
 import { getUserSaveData } from '@renderer/lib/utils';
@@ -7,6 +7,7 @@ import { useState } from 'react';
 import SortersModal from './components/sorters-modal';
 import ViewersModal from './components/viewers-modal';
 import { buttonVariants } from '@renderer/components/ui/button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface SelectorsPageProps {
   selectorType: 'sorters' | 'viewers';
@@ -20,10 +21,21 @@ export default function SelectorsPage({ selectorType }: SelectorsPageProps): Rea
 
   const SortersButtons = savedSorters.map((sorterName) => {
     return (
-      <div key={sorterName} className="bg-primary">
-        {editingMode && <DialogTrigger className={buttonVariants({ variant: 'default' })}>^</DialogTrigger>}
-        <Button disabled={editingMode}>{sorterName}</Button>
-        {editingMode && <DialogTrigger className={buttonVariants({ variant: 'destructive' })}>X</DialogTrigger>}
+      <div key={sorterName} className="bg-primary w-full flex flex-row justify-center">
+        {editingMode && <DialogTrigger className={buttonVariants({ variant: 'default' }) + ' w-1/6'}></DialogTrigger>}
+        <Button
+          disabled={editingMode}
+          className="
+        w-4/6
+        "
+        >
+          {sorterName}
+        </Button>
+        {editingMode && (
+          <DialogTrigger className={buttonVariants({ variant: 'destructive' }) + ' w-1/6'}>
+            <FontAwesomeIcon icon={byPrefixAndName.fas['xmark']} />
+          </DialogTrigger>
+        )}
       </div>
     );
   });
@@ -31,12 +43,32 @@ export default function SelectorsPage({ selectorType }: SelectorsPageProps): Rea
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-y-6">
       <Dialog>
+        {/* Modal for the associated selector */}
         {selectorType === 'sorters' ? <SortersModal /> : <ViewersModal />}
-        <Card className="w-1/2 p-2 h-1/4 max-h-1/4">
-          <CardContent className="flex flex-col items-center gap-2">{SortersButtons}</CardContent>
-          <CardFooter></CardFooter>
+
+        {/* Main card displayed on the page */}
+        <Card
+          className="bg-secondary p-2 h-1/4 max-h-1/4
+          w-78
+          md:w-86
+          lg:w-96
+        "
+        >
+          <CardContent
+            className="flex flex-col items-center h-full max-h-full overflow-x-hidden overflow-y-auto scrollbar border-2 border-background p-0
+        "
+          >
+            {SortersButtons}
+          </CardContent>
         </Card>
-        <div className="flex flex-row justify-between w-1/3">
+
+        {/* Container for the buttons on the page */}
+        <div
+          className="flex flex-row justify-between 
+        w-78
+      md:w-86
+      lg:w-96"
+        >
           {editingMode ? (
             <>
               <Button onClick={toggleEditingMode}>Save</Button>
