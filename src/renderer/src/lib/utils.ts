9c1149d82caf12ce17c77702bcff99b6c54cf109
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2026-01-02 11:38:11 -0800

    Added a title case function

--- src/renderer/src/lib/utils.ts
+++ src/renderer/src/lib/utils.ts
@@ -6,6 +6,17 @@ export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
 
+export function getTitleCase(str: string): string {
+  const strArray = str.split(' ');
+  const titleArray: string[] = [];
+
+  for (const string of strArray) {
+    titleArray.push(string.at(0)?.toUpperCase() + string.slice(1, string.length));
+  }
+
+  return titleArray.join(' ');
+}
+
 export function getUniqueID(): string {
   // Credit: MrCano369x
   // https://stackoverflow.com/questions/3231459/how-can-i-create-unique-ids-with-javascript
