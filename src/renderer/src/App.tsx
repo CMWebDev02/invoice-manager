83af488b6e95a2b76b9c677be36932a3c3796579
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2026-01-02 12:11:01 -0800

    Removed unneeded import

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import './styles/styles.css';
 import TitlePage from './pages/title-page/title-page';
-import NavBar from './components/user/nav-bar';
 import SettingsPage from './pages/settings/settings-page';
 import ChangeLogsPage from './pages/changelogs/changelogs-pages';
 import SelectorsPage from './pages/selectors/selectors-page';
@@ -35,6 +34,7 @@ export default function App(): React.JSX.Element {
             <Route path="/changelogs" element={<ChangeLogsPage />} />
             <Route path="/settings" element={<SettingsPage />} />
           </Route>
+
           <Route path="/sorters/:sorterId" element={<SortersPage />} />
           {/* <Route path="/viewers/:sorterId" element={<SortersPage />} /> Add once viewers page is initialized*/}
         </Routes>
