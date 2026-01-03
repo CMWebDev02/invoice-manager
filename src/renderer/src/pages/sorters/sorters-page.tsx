61b56165608cf142b72b6522a911a51019a8db24
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2026-01-03 11:55:58 -0800

    Added the year selector

--- src/renderer/src/pages/sorters/sorters-page.tsx
+++ src/renderer/src/pages/sorters/sorters-page.tsx
@@ -2,12 +2,12 @@ import { searchSelector } from '@renderer/lib/store';
 import SorterContainer from './containers/sorter-container';
 import { useParams } from 'react-router';
 
-// Get the sorterId upon loading the page,
-// validate the id,
-// if not valid return an error
-// else search for the selector and get its associated information
-// validate the invoices destination and the directories destination,
-// validate the letter folders within the directories destination
+//// Get the sorterId upon loading the page,
+//// validate the id,
+//// if not valid return an error
+//// else search for the selector and get its associated information
+//// validate the invoices destination and the directories destination,
+//// validate the letter folders within the directories destination
 // gather the file names within each letter folder
 // gather the current invoice within the invoice destination, each time a transfer occurs reacquire the current file value
 // filter the current directories available to the user based on their search, match from start only
