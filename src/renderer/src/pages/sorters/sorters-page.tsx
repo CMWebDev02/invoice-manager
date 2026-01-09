468c35f5e2139cea813c90e1867cc899ac266655
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2026-01-09 13:00:41 -0800

    Added todo list

--- src/renderer/src/pages/sorters/sorters-page.tsx
+++ src/renderer/src/pages/sorters/sorters-page.tsx
@@ -2,17 +2,11 @@ import { searchSelector } from '@renderer/lib/store';
 import SorterContainer from './containers/sorter-container';
 import { useParams } from 'react-router';
 
-//// Get the sorterId upon loading the page,
-//// validate the id,
-//// if not valid return an error
-//// else search for the selector and get its associated information
-//// validate the invoices destination and the directories destination,
-//// validate the letter folders within the directories destination
-//// gather the file names within each letter folder
-//// gather the current invoice within the invoice destination, each time a transfer occurs reacquire the current file value
-//// filter the current directories available to the user based on their search, match from start only
-// highlight and store the current directory the user selects
-// transfer the current invoice to the currently selected directory
+// TODO Trigger a refresh of the current invoice upon successful file transfer
+// TODO Rename any invoice vars to file
+// TODO Add the new directory modal and logic
+// TODO Add the error popups
+// TODO Review all functions and revalidate their error checking and return values
 
 export default function SortersPage(): React.JSX.Element {
   const { sorterId } = useParams();
