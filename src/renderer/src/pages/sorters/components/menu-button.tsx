62a704ac86f9349942482279ef7d60c366fd44ae
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2026-01-30 14:01:57 -0800

    Fixed the drop down menu

--- src/renderer/src/pages/sorters/components/menu-button.tsx
+++ src/renderer/src/pages/sorters/components/menu-button.tsx
@@ -1,16 +1,16 @@
-import { Button } from '@renderer/components/ui/button';
+import { buttonVariants } from '@renderer/components/ui/button';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@renderer/components/ui/dropdown-menu';
 import ButtonLink from '@renderer/components/user/button-link';
 
 export default function MenuButton(): React.JSX.Element {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Button>Menu</Button>
+      <DropdownMenuTrigger type="button" className={buttonVariants()}>
+        Menu
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem className="w-full">
-          <ButtonLink linkHref="/" className='w-full'>
+          <ButtonLink linkHref="/" className="w-full">
             Return
           </ButtonLink>
         </DropdownMenuItem>
