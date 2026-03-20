7a916e8da2121fa79ce28f7358693b5a307ae1ad
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2026-03-20 11:16:40 -0700

    Added the error page to the viewers page

--- src/renderer/src/pages/viewers/viewers-page.tsx
+++ src/renderer/src/pages/viewers/viewers-page.tsx
@@ -2,15 +2,24 @@ import { useParams } from 'react-router';
 import ViewerContainer from './containers/viewer-container';
 import useViewerClassInit from '@renderer/hooks/useViewerClassInit';
 import LoadingPage from '../loading/loading-page';
+import ErrorPage from '@renderer/components/pages/error-page';
 
 export default function ViewersPage(): React.JSX.Element {
   const { viewerId } = useParams();
   const { viewerActions, isLoading, error } = useViewerClassInit({ viewerId, asyncFunctionKey: 'sorting-class' });
 
+  if (error !== null) {
+    return <ErrorPage errors={[error]} />;
+  }
+
+  if (viewerActions === null || viewerActions === undefined) {
+    return <ErrorPage errors={[new Error('Failed to Access Sorter!')]} />;
+  }
+
   return (
     <>
       {isLoading && <LoadingPage />}
-      {viewerActions === null || viewerActions === undefined ? <h1>Error: {error?.message}</h1> : <ViewerContainer viewerActions={viewerActions} />}
+      <ViewerContainer viewerActions={viewerActions} />
     </>
   );
 }
